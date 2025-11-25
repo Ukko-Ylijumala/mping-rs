7a90f7af2a7094e6c554d60d45dc60d18b11a20b
Mikko Tanner <mikko.tanner@zoner.fi>
2025-11-25 14:33:33 +0200

    structs: add StatsSnapshot and its implementation. This struct simplifies the statistics gathering and display in main app by a large margin.

--- src/structs.rs
+++ src/structs.rs
@@ -3,9 +3,11 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use crate::latencywin::LatencyWindow;
-use std::{fmt::Display, net::IpAddr};
+use std::{fmt::Display, net::IpAddr, time::Instant};
 use surge_ping::SurgeError;
 use tokio::sync::Mutex;
+
+const MICRO_TO_MILLI: f64 = 1e3;
 
 #[derive(Debug)]
 pub(crate) enum PingStatus {
@@ -42,4 +44,108 @@ pub(crate) struct PingTargetInner {
 pub(crate) struct PingTarget {
     pub addr: IpAddr,
     pub data: Mutex<PingTargetInner>,
+}
+
+/// Snapshot of ping statistics at a point in time.
+///
+/// Includes stringifying methods for display purposes.
+/// Unless otherwise noted, RTT values are stored as
+/// microseconds and displayed as milliseconds.
+#[derive(Debug)]
+pub(crate) struct StatsSnapshot {
+    pub sent: u64,
+    pub recv: u64,
+    pub min: Option<u32>,
+    pub max: Option<u32>,
+    pub mean: Option<f64>,
+    pub last: Option<u32>,
+    pub stdev: Option<f64>,
+    pub when: Instant,
+}
+
+impl StatsSnapshot {
+    /// Extract a [StatsSnapshot] from [PingTargetInner]
+    pub fn new_from(data: &PingTargetInner) -> Self {
+        let (mean, min, max) = match data.rtts.mean_min_max() {
+            Ok((mean, mi, ma)) => (Some(mean), Some(mi), Some(ma)),
+            Err(_) => (None, None, None),
+        };
+        StatsSnapshot {
+            when: Instant::now(),
+            sent: data.sent,
+            recv: data.recv,
+            mean,
+            min,
+            max,
+            last: match data.rtts.last() {
+                Ok(v) => Some(v),
+                Err(_) => None,
+            },
+            stdev: match data.rtts.stdev() {
+                Ok(v) => Some(v),
+                Err(_) => None,
+            },
+        }
+    }
+
+    /// Packet loss percentage as a float `[0.0 .. 1.0]`.
+    pub fn loss(&self) -> f64 {
+        if self.sent == 0 {
+            0.0
+        } else {
+            (self.sent - self.recv) as f64 / self.sent as f64
+        }
+    }
+
+    /// Packet loss as formatted string.
+    pub fn loss_str(&self) -> String {
+        if self.sent == 0 {
+            "-".to_string()
+        } else if (self.sent - self.recv) == 1 {
+            // catch the common case of one receive missing (probably in transit)
+            "0.0%".to_string()
+        } else {
+            format!("{:.1}%", 1e2 * self.loss())
+        }
+    }
+
+    /// Minimum RTT as formatted string (as milliseconds).
+    pub fn min_str(&self) -> String {
+        match self.min {
+            Some(v) => format!("{:.2}", v as f64 / MICRO_TO_MILLI),
+            None => "-".to_string(),
+        }
+    }
+
+    /// Maximum RTT as formatted string (as milliseconds).
+    pub fn max_str(&self) -> String {
+        match self.max {
+            Some(v) => format!("{:.2}", v as f64 / MICRO_TO_MILLI),
+            None => "-".to_string(),
+        }
+    }
+
+    /// Last RTT as formatted string (as milliseconds).
+    pub fn last_str(&self) -> String {
+        match self.last {
+            Some(v) => format!("{:.2}", v as f64 / MICRO_TO_MILLI),
+            None => "-".to_string(),
+        }
+    }
+
+    /// Mean RTT as formatted string (as milliseconds).
+    pub fn mean_str(&self) -> String {
+        match self.mean {
+            Some(v) => format!("{:.2}", v / MICRO_TO_MILLI),
+            None => "-".to_string(),
+        }
+    }
+
+    /// Standard deviation as formatted string (as milliseconds).
+    pub fn stdev_str(&self) -> String {
+        match self.stdev {
+            Some(v) => format!("{:.2}", v / MICRO_TO_MILLI),
+            None => "-".to_string(),
+        }
+    }
 }
