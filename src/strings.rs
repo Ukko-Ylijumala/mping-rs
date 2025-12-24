701cacaeaa5235ee0aeab2f85b78053f1164e523
Mikko Tanner <mikko.tanner@gmail.com>
2025-12-24 16:08:11 +0200

    strings: add F12 keybinding to display application log message buffer in a popup

--- src/strings.rs
+++ src/strings.rs
@@ -75,7 +75,7 @@ pub static INFO_CAPS_V4: &str = "\nFor IPv4 only you can also allow group IDs sy
     sudo sysctl -w net.ipv4.ping_group_range=\"<start> <end>\"\n";
 
 // App keybindings help text
-pub static HELP_KEYS: [[&str; 2]; 16] = [
+pub static HELP_KEYS: [[&str; 2]; 17] = [
     ["q, CTRL-C",      "Quit the program"],
     ["h, F1",          "Show/hide this help screen"],
     ["Up, Down",       "Scroll target list up/down"],
@@ -92,4 +92,5 @@ pub static HELP_KEYS: [[&str; 2]; 16] = [
     ["Delete",         "Stop pinging the selected target and remove it"],
     ["CTRL+Delete",    "Stop and remove all unreachable targets"],
     ["F10",            "Toggle \"perf\" mode (reduce task spawn overhead)"],
+    ["F12",            "Display application log message buffer in a popup"],
 ];
